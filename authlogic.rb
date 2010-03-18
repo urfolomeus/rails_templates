cb161f25b2365be9db2515e4f57b9f04d550282a
Alan Gardner <urfolomeus@gmail.com>
2010-03-18 20:39:46 +0000

    replacing restful_authentication with authlogic

--- authlogic.rb
+++ authlogic.rb
@@ -3,4 +3,38 @@ if yes?("Install gem? [y/n]")
   rake "gems:install"
 end
 
-generate "nifty_scaffold user username:string email:string password:string new edit"
+user_fields = [ "username:string", 
+                "email:string", 
+                "crypted_password:string",
+                "password_salt:string",
+                "persistence_token"]
+user_controller_actions = ["new", "edit"]
+
+generate "nifty_scaffold user #{user_fields.join('')} #{user_controller_actions.join('')}"
+
+file 'app/models/user.rb', 
+%q{
+class User < ActiveRecord::Base
+  acts_as_authentic
+end
+}
+
+file 'app/views/users/_form.html.erb', 
+%q{
+<% form_for @user do |f| %>
+  <%= f.error_messages %>
+  <p>
+    <%= f.label :username %><br />
+    <%= f.text_field :username %>
+  </p>
+  <p>
+    <%= f.label :email %><br />
+    <%= f.text_field :email %>
+  </p>
+  <p>
+    <%= f.label :password %><br />
+    <%= f.text_field :password %>
+  </p>
+  <p><%= f.submit "Submit" %></p>
+<% end %>
+}
